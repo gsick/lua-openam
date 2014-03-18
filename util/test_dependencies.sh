74c43b15f0a179fa3acf39c6f04401fafccb2921
gsick <gsick@emailvision.com>
2014-03-18 10:47:49 +0100

    add util file

---
+++ util/test_dependencies.sh
@@ -0,0 +1,20 @@
+# Memo
+
+cd /tmp
+wget https://github.com/pintsized/lua-resty-http/archive/v0.02.tar.gz
+tar -zxvf v0.02.tar.gz
+cd lua-resty-http-0.02
+make PREFIX=/usr LUA_LIB_DIR=/usr/lib64/lua/5.1 install
+
+cd /tmp
+wget http://www.kyne.com.au/~mark/software/download/lua-cjson-2.1.0.tar.gz
+tar -zxvf lua-cjson-2.1.0.tar.gz
+cd lua-cjson
+make
+cp cjson.so /usr/lib64/lua/5.1
+
+cd /tmp
+git clone https://github.com/starwing/luautf8.git
+cd luautf8
+gcc -shared -fPIC -O3 lutf8lib.c -o utf8.so
+cp utf8.so /usr/lib64/lua/5.1/
