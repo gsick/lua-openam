a22b70ddc5733dcd51e0e57f8a17cc8b68923892
gsick <gsick@emailvision.com>
2014-03-18 11:06:41 +0100

    update util

---
+++ util/test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+TEST_NGINX_OPENAM_URI="http://....:.../openam" \
+TEST_NGINX_OPENAM_USER="..." \
+TEST_NGINX_OPENAM_PWD="..." \
+TEST_NGINX_OPENAM_USER_TEST="must be different than the first user" \
+TEST_NGINX_OPENAM_PWD_TEST="..." \
+TEST_NGINX_OPENAM_REALM="realm of the user_test e.g. /test" \
+TEST_NGINX_OPENAM_URL_AUTHORIZE="http://..." \
+TEST_NGINX_OPENAM_URL_UNAUTHORIZE="http://..." \
+prove ../t/*.t
--- util/test_dependencies.sh
+++ util/test_dependencies.sh
@@ -6,11 +6,12 @@ wget https://github.com/pintsized/lua-resty-http/archive/v0.03.tar.gz
 tar -zxvf v0.03.tar.gz
 cd lua-resty-http-0.03
 make PREFIX=/usr LUA_LIB_DIR=/usr/lib64/lua/5.1 install
+cp /usr/lib64/lua/5.1/resty/http/http.lua /usr/lib64/lua/5.1/resty
 
 cd /tmp
 wget http://www.kyne.com.au/~mark/software/download/lua-cjson-2.1.0.tar.gz
 tar -zxvf lua-cjson-2.1.0.tar.gz
-lua-cjson-2.1.0
+cd lua-cjson-2.1.0
 make
 cp cjson.so /usr/lib64/lua/5.1
 
@@ -18,4 +19,5 @@ cd /tmp
 git clone https://github.com/starwing/luautf8.git
 cd luautf8
 gcc -shared -fPIC -O3 lutf8lib.c -o utf8.so
-cp utf8.so /usr/lib64/lua/5.1/
+cp utf8.so /usr/lib64/lua/5.1
+
